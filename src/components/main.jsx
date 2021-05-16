4c57b49830ba2fca61e02d947afbd4389a6158c8
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2021-05-17 01:42:59 +0900

    navigation bar

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import './main.css'
+import LOGO_Full from "../perfume_pictures/LOGO_Full.PNG"
+import MAIN_PAGE from "../perfume_pictures/MAIN_PAGE.png"
 
 const main = () => {
 
@@ -8,7 +11,20 @@ const main = () => {
 
     return (
         <div>
-            <button onClick = {handleclick}>Find My Perfume</button>
+            <img src={LOGO_Full} alt = "" className='logo'/>
+            <ul>
+                <li><a href="/">HOME</a></li>
+                <li><a href="/">ABOUT</a></li>
+                <li><a href="/">BEST</a></li>
+                <li><a href="./filter">DISCOVER</a></li>
+                <li><a href="/">SIGN IN</a></li>
+                <ul style={{float: "right"}}>
+                    <li><a href="/bbs/login.php">LOG IN</a></li>
+                    <li><a href="/bbs/register_form.php">SIGN UP</a></li>
+                </ul>
+            </ul>
+            <img src={MAIN_PAGE} alt = "" className='main'/>
+            <br/>
         </div>
     )
 }
