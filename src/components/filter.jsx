6bdfc73399bfaab6c9fce4feca209774f93590f1
ZiehoChoi <legitimation@kaist.ac.kr>
2021-05-16 21:38:09 +0900

    rough skeleton code, algorithm tbd

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -11,8 +11,58 @@ const filter = () => {
         <div>
             <div>filter</div>
             <button onClick = {handleclick}>perfume info</button>
+        
+
+
+        // follow background html, css form from the main page
+
+        //Tab for women, men
+
+        <table> Women / Men </table>
+
+        //like figma implementation
+
+        <h1> PERFUME FINDER </h1>
+        <h2> Choose scents to avoid: </h2>
+
+        //filtering choices, enable multiple selection 
+
+        <li class="choice0">
+        <input name="input0" type="checkbox" value="0" id="choice_citrus"></input>
+        <label for="choice_citrus" id="Citrus"> Citrus </label>
+        </li>
+        <li class="choice1">
+        <input name="input1" type="checkbox" value="1" id="choice_fruity"></input>
+        <label for="choice_fruity" id="Fruity"> Fruity </label>
+        </li> 
+        <li class="choice2">
+        <input name="input2" type="checkbox" value="2" id="choice_flowerly"></input>
+        <label for="choice_flowerly" id="flowerly"> flowerly</label>
+        </li>  
+        <li class="choice3">
+        <input name="input3" type="checkbox" value="3" id="choice_woody"></input>
+        <label for="choice_woody" id="woody"> woody</label>
+        </li>  
+        <li class="choice4">
+        <input name="input4" type="checkbox" value="4" id="choice_oriental"></input>
+        <label for="choice_oriental" id="oriental"> oriental</label>
+        </li>
+
+        {/* // showing image of perfumes, clickable --> perfume info --> */}
+
+        <div class="cell box" style="flex-grow: 0;">
+        <div class="card-section">
+        <img src="https://.jpg"></img> 
+        <div class="card-section"><p><a href="https://www.html"> Name of perfume
+        <span class="link-span"></span></a></p> 
+        <p><small> Type </small></p></div></div>
         </div>
-    )
-}
+
+        // SEARCH BAR(?)
+        // JS filtering function
+
+        </div>
+            )
+        }
 
 export default filter
