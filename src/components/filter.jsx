d0152a3129007d64711e9312b0a3fbab4deeb3a3
ZiehoChoi <legitimation@kaist.ac.kr>
2021-05-19 01:52:21 +0900

    filter 5case ver. checkbox working

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -7,62 +7,57 @@ const filter = () => {
     }
 
     return (
-        <div></div>
-        
-        // <div>
-        //     <div>filter</div>
-        //     <button onClick = {handleclick}>perfume info</button>
+        <div>
+        <button onClick = {handleclick}> perfume info </button>
         
 
+        <div> filter </div>
+        <h1> PERFUME FINDER </h1>
+        <h2> Choose scents to avoid: </h2>
 
-        // // follow background html, css form from the main page
+        <li class="choice0">
+        <input name="input0" type="checkbox" value="0" id="choice_citrus"></input>
+        <label for="choice_citrus" id="Citrus"> Citrus </label>
+        </li>
 
-        // // Tab for women, men
+        <li class="choice1">
+        <input name="input1" type="checkbox" value="1" id="choice_fruity"></input>
+        <label for="choice_fruity" id="Fruity"> Fruity </label>
+        </li>
 
-        // <table> Women / Men </table>
+        <li class="choice2">
+        <input name="input2" type="checkbox" value="2" id="choice_flowerly"></input>
+        <label for="choice_flowerly" id="flowerly"> flowerly </label>
+        </li>
 
-        // //like figma implementation
+        <li class="choice3">
+        <input name="input3" type="checkbox" value="3" id="choice_woody"></input>
+        <label for="choice_woody" id="woody"> woody </label>
+        </li>
 
-        // <h1> PERFUME FINDER </h1>
-        // <h2> Choose scents to avoid: </h2>
+        <li class="choice4">
+        <input name="input4" type="checkbox" value="4" id="choice_oriental"></input>
+        <label for="choice_oriental" id="oriental"> oriental </label>
+        </li>
 
-        // //filtering choices, enable multiple selection 
+        {/* <div class="cell box" style="flex-grow: 0;">
+        <div class="card-section"> */}
 
-        // <li class="choice0">
-        // <input name="input0" type="checkbox" value="0" id="choice_citrus"></input>
-        // <label for="choice_citrus" id="Citrus"> Citrus </label>
-        // </li>
-        // <li class="choice1">
-        // <input name="input1" type="checkbox" value="1" id="choice_fruity"></input>
-        // <label for="choice_fruity" id="Fruity"> Fruity </label>
-        // </li> 
-        // <li class="choice2">
-        // <input name="input2" type="checkbox" value="2" id="choice_flowerly"></input>
-        // <label for="choice_flowerly" id="flowerly"> flowerly</label>
-        // </li>  
-        // <li class="choice3">
-        // <input name="input3" type="checkbox" value="3" id="choice_woody"></input>
-        // <label for="choice_woody" id="woody"> woody</label>
-        // </li>  
-        // <li class="choice4">
-        // <input name="input4" type="checkbox" value="4" id="choice_oriental"></input>
-        // <label for="choice_oriental" id="oriental"> oriental</label>
-        // </li>
+     
+        {/* <img src="https://.jpg"></img>
+        <div class="card-section"><p><a href="https://www.html"> Name of perfume 
+        
 
-        // {/* // showing image of perfumes, clickable --> perfume info --> */}
+        <span class="link-span"></span></a></p> 
+        <p><small> Type </small></p>
 
-        // <div class="cell box" style="flex-grow: 0;">
-        // <div class="card-section">
-        // <img src="https://.jpg"></img> 
-        // <div class="card-section"><p><a href="https://www.html"> Name of perfume
-        // <span class="link-span"></span></a></p> 
-        // <p><small> Type </small></p></div></div>
-        // </div>
+        </div> */}
 
-        // // SEARCH BAR(?)
-        // // JS filtering function
+        {/* </div>
+        </div> */}
 
-        // </div>
+        </div>
+        
 
             )
         }
