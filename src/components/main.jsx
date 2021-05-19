54fcb2405e7256ddf085404e2a518f66ca76bcca
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2021-05-20 08:25:20 +0900

    fixed small error

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -62,10 +62,10 @@ class main extends Component {
                         <li><a href="/">BRANDS</a></li>
                         <li><a href="./filter">DISCOVER</a></li>
                         <ul style={{float: "right"}}>
-                            <li className="dropdown signin"><a><i class="fas fa-user-circle fa-lg"></i>&nbsp;&nbsp;{user_name}</a>
+                            <li className="dropdown signin"><a><i className="fas fa-user-circle fa-lg"></i>&nbsp;&nbsp;{user_name}</a>
                                 <div className="dropdown-content">
                                     <a href="/">My Page</a>
-                                    <a onClick={signOutWithGoogle}>Sign Out</a>
+                                    <a onClick={() => {signOutWithGoogle(); window.location.reload();}}>Sign Out</a>
                                 </div>
                             </li>
                         </ul>
