c3d32887fc1f8096f2d7c5c33e1ed17e2ab09889
Junho Myung <junho00211@kaist.ac.kr>
2021-05-25 01:07:32 +0900

    review error fixeD

--- src/components/music.jsx
+++ src/components/music.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     MusicH1,
     MusicH2,
@@ -14,6 +14,7 @@ import Fade from '@material-ui/core/Fade';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import firebase from './firebase';
+import { SettingsInputAntenna } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -41,8 +42,9 @@ export default function Music() {
     const [artist, setArtist] = useState('')
     const [url, setURL] = useState('')
     const [description, setDescription] = useState('')
-    const review_order = [];
-    const key_order = [];
+    const [update, setUpdate] = useState(0)
+    const [review_order, setReviewOrder] = useState([]);
+    const [key_order, setKeyOrder] = useState([]);
 
     const handleOpen = () => {
         setOpen(true);
@@ -69,7 +71,6 @@ export default function Music() {
     }
 
     const handleVote = (value, index) => {
-        //console.log([...key_order].reverse()[index])
         if (firebase.auth().currentUser === null){
             alert("Please sign in to vote")
         }
@@ -86,6 +87,7 @@ export default function Music() {
             updates['/'+ perfume_name +'/' + [...key_order].reverse()[index]] = new_data;
             firebase.database().ref().update(updates);
             firebase.database().ref('/'+ perfume_name +'/'+ [...key_order].reverse()[index] + '/voteID/').push(firebase.auth().currentUser.email);
+            setUpdate(update => update + 1)
         }
         else {
             var new_data = {
@@ -103,12 +105,12 @@ export default function Music() {
             temp.get().then((snapshot) =>{
                 var keys = Object.keys(snapshot.val());
                 snapshot.forEach((child) => {
-                    //console.log(child.val())
                     if(child.val() === firebase.auth().currentUser.email){
                         firebase.database().ref('/'+ perfume_name +'/'+ [...key_order].reverse()[index] + '/voteID/' + child.key).remove()
                     }
                 })
             })
+            setUpdate(update => update + 1)
         }
     }
 
@@ -136,19 +138,28 @@ export default function Music() {
     }
 
     const sortbyvote = () => {
+        var temp1 = []
+        var temp2 = []
         firebase.database().ref('/'+ perfume_name +'/').orderByChild('vote').on("child_added", function(snapshot) {
-            review_order.push(snapshot.val());
-            key_order.push(snapshot.key);
+            temp1.push(snapshot.val())
+            temp2.push(snapshot.key)
+            // review_order.push(snapshot.val());
+            // key_order.push(snapshot.key);
         });
-
+        setReviewOrder(temp1)
+        setKeyOrder(temp2)
     }
 
+    useEffect(() => {
+        sortbyvote()
+    })
+    
 
+    
 
     return (
         <div>
             <MusicH1>Top Recommendations</MusicH1>
-            {sortbyvote()}
             {[...review_order].reverse().map((value, index) => {
             return (
                 <ReviewTable  width = "80%">
