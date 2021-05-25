17b4124eafcf0c8b31073561a9809309897b6759
Junho Myung <junho00211@kaist.ac.kr>
2021-05-26 08:57:45 +0900

    shoppingcart icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,20 @@ import LOGO_Full from "../perfume_pictures/LOGO_Full.PNG"
 import 'firebase/auth';
 import {auth, signInWithGoogle, signOutWithGoogle} from "./firebase.jsx";
 import {Link} from 'react-router-dom'
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}))(Badge);
+
 
 export class NavBar extends Component {
     constructor() {
@@ -53,7 +67,13 @@ export class NavBar extends Component {
                         <ul style={{float: "right"}}>
                             <li className="dropdown signin"><a href="# "><i className="fas fa-user-circle fa-lg"></i>&nbsp;&nbsp;{user_name}</a>
                                 <div className="dropdown-content">
-                                    <a href="/">My Page</a>
+                                    <a href="/">My Page 
+                                        <IconButton aria-label="cart">
+                                            <StyledBadge badgeContent={4} color="secondary">
+                                            <ShoppingCartIcon />
+                                            </StyledBadge>
+                                        </IconButton>
+                                    </a>
                                     <a onClick={() => {signOutWithGoogle(); window.location.reload();}} href="# ">Sign Out</a>
                                 </div>
                             </li>
