dc4e9f2aca295c5d6513d3e57b47955d4d21b435
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2021-05-30 02:09:57 +0900

    trial 1

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 //import "bulma/css/bulma.css";
-import main from "./components/main";
-import filter from "./components/filter";
-import visual from "./components/visual";
-import mypage from "./components/mypage";
+import Main from "./components/main";
+import Filter from "./components/filter";
+import Visual from "./components/visual";
+import Mypage from "./components/mypage";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/filter" component = {filter} />
-      <Route path="/visual" component = {visual} />
-      <Route exact path="/" component = {main}/>
-      <Route path="/mypage" component = {mypage} />
+      <Route path="/filter" component = {Filter} />
+      <Route path="/visual" component = {Visual} />
+      <Route exact path="/" component = {Main}/>
+      <Route path="/mypage" component = {Mypage} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
